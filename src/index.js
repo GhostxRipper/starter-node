8fc68b04c3872c38cdb9ab4c2c6215bca2ce7e2c
Yann Amsellem <y.amsellem@inextenso.digital>
2018-02-07 09:54:05 +0100

    Oops 😅

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { createServer } from 'http'
 
+const port = process.env.PORT || 3000
+
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' })
   res.end('okay')
 })
 
-server.listen(3000, () => {
-  console.log(`server running on port: ${server.addresse().port}`)
+server.listen(port, () => {
+  console.log(`server running on port: ${server.address().port}`)
 })
